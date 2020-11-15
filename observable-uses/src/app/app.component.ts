4156c0862e172dc7c478ba56fcccfd5768144079
Paola Monteiro <paoolamonteiro98@gmail.com>
2020-11-15 15:34:37 -0300

    added more observable usage examples

--- observable-uses/src/app/app.component.ts
+++ observable-uses/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Subscription, Observable, from, of } from 'rxjs';
-import { filter, first, flatMap, map, pluck, reduce, take, tap } from 'rxjs/operators';
+import { Subscription, Observable, from, of, merge, partition } from 'rxjs';
+import { concatAll, concatMap, count, filter, first, flatMap, last, map, window, mergeMap, pluck, reduce, take, tap } from 'rxjs/operators';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from "@angular/forms";
 import { MessageService } from "./services/message.service";
@@ -34,7 +34,6 @@ export class AppComponent implements OnInit, OnDestroy {
     this.subscriptions.add(
       this.messageService.getSeries().pipe(
         flatMap(series => series),
-        pluck('actors'),
         map((series: any[]) => series.map(serie => serie.actors)),
         map((actors: string[]) => actors.join(',')), // transforms actors array in a string of actors
         map((actors: string) => actors.split(',')), // turns back the string of actors to an array of actors
@@ -58,23 +57,49 @@ export class AppComponent implements OnInit, OnDestroy {
       first()
     ).subscribe(value => console.log('reduce and first ', value))
 
-    // ***************************************
-
     // Getting data with pluck function
     this.subscriptions.add(
       this.messageService.getSeries().pipe(
         flatMap(series => series),
+        last(),
         pluck('actors'),
-      ).subscribe(length => console.log(length)) // 16
+      ).subscribe(actors => console.log('pluck ', actors))
     )
 
-    // ***************************************
+    // Ways to flattening higher-order streams:
 
-    
+    // Getting data with map + concatAll functions
+    this.getAllSeries().pipe(
+      map(value => value),
+      tap(console.log),
+      concatAll(),
+    ).subscribe(values => console.log('map + concatAll ', values))
+
+    // Getting data with concatMap function
+    this.getAllSeries().pipe(
+      concatMap(value => value),
+    ).subscribe(values => console.log('concatMap ', values)) // same result of previous function
+
+    // Getting data with mergeMap function
+    this.getAllSeries().pipe(
+      mergeMap(value => value),
+    ).subscribe(values => console.log('mergeMap ', values)) // same result of previous function
+
+    // Getting data with count function
+    this.subscriptions.add(
+      this.messageService.getSeries().pipe(
+      count(),
+    ).subscribe(values => console.log('count ', values))) // 1
   }
 
   public setMessage(): void {
     this.messageService.setMessage(this.form.get('message').value);
+  }
+
+  // High Order Obsevables:
+  public getAllSeries = () => {
+    // return of(this.messageService.getSeries());
+    return of([of(this.messageService.getSeries()), of(this.messageService.getSeries())])
   }
 
   ngOnDestroy() {
